e9a2cefc231e601a3db389161f0294dd5959e7d6
Oz Tamir <theoztamir@gmail.com>
2020-07-05 11:51:43 +0300

    Rewored the site
    It's now based of of a template from https://bootstrapmade.com/, though I've heavily modified it to be very minimal

--- README.md
+++ README.md
@@ -3,13 +3,7 @@ OzTamir.com
 
 #### The repo for my personal website, [OzTamir.com] (http://oztamir.com)
 
-Simple, sleek, responsive - just the way I like it.
+Basiclly a modified version of [MyResume] (https://bootstrapmade.com/free-html-bootstrap-template-my-resume/), see credits.txt for more info.
 
 ## Libraries in use:
- - [Twitter's Bootstrap] (http://getbootstrap.com/) for HTML and responsiveness across devices.
- - [move.js] (https://github.com/visionmedia/move.js) for the cool effect when clicking one of the social icons.
- - [animate.css] (https://github.com/daneden/animate.css) for the little fade in-out.
- - [Font Awesome] (https://github.com/FortAwesome/Font-Awesome) for the social icons.
  - [BOOTSTRA.386] (https://github.com/kristopolous/BOOTSTRA.386) used in the 404 page.
-
-Copyright 2015 Oz Tamir
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/**
+* Template Name: MyResume - v2.1.0
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+
+  // Initiate aos_init() function
+  aos_init();
+
+})(jQuery);
